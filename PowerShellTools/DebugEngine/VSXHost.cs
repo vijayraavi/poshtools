62e5de7e5282d4c4db0536dbc576a10974ff8fc1
adamdriscoll <adam.r.driscoll@gmail.com>
2014-05-03 13:46:49 -0500

    Code cleanup & fixing prompt

--- PowerShellTools/DebugEngine/VSXHost.cs
+++ PowerShellTools/DebugEngine/VSXHost.cs
@@ -20,43 +20,15 @@ namespace PowerShellTools.DebugEngine
     using IReplWindow = IPowerShellReplWindow;
 #endif
 
+    /// <summary>
+    ///     The PoshTools PowerShell host
+    /// </summary>
     public class VSXHost : PSHost, IHostSupportsInteractiveSession
     {
-        private Runspace _runspace;
-        private Guid _instanceId = Guid.NewGuid();
-        public HostUi HostUi { get; private set; }
-
-        public static VSXHost Instance { 
-            get;
-            private set;
-        }
-
-        public IReplWindow ReplWindow
-        {
-            get { return HostUi.ReplWindow; }
-            set
-            {
-                HostUi.ReplWindow = value;
-                if (value != null)
-                {
-                    RefreshPrompt();
-                }
-            }
-        }
-
-        /// <summary>
-        /// The culture information of the thread that created
-        /// this object.
-        /// </summary>
-        private CultureInfo originalCultureInfo =
-            System.Threading.Thread.CurrentThread.CurrentCulture;
-
-        /// <summary>
-        /// The UI culture information of the thread that created
-        /// this object.
-        /// </summary>
-        private CultureInfo originalUICultureInfo =
-            System.Threading.Thread.CurrentThread.CurrentUICulture;
+        private readonly Guid _instanceId = Guid.NewGuid();
+        private readonly CultureInfo _originalCultureInfo = Thread.CurrentThread.CurrentCulture;
+        private readonly CultureInfo _originalUiCultureInfo = Thread.CurrentThread.CurrentUICulture;
+        private readonly Runspace _runspace;
 
         public VSXHost(bool overrideExecutionPolicy = false)
         {
@@ -82,12 +54,90 @@ namespace PowerShellTools.DebugEngine
             }
         }
 
+        public VSXHost(Runspace runspace)
+        {
+            _runspace = runspace;
+        }
+
+        public HostUi HostUi { get; private set; }
+
+        /// <summary>
+        ///     The main PowerShell host for the extension.
+        /// </summary>
+        public static VSXHost Instance { get; private set; }
+
+        public IReplWindow ReplWindow
+        {
+            get { return HostUi.ReplWindow; }
+            set
+            {
+                HostUi.ReplWindow = value;
+                if (value != null)
+                {
+                    RefreshPrompt();
+                }
+            }
+        }
+
+        public override string Name
+        {
+            get { return "PowerShell Tools for Visual Studio"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+
+        public override Guid InstanceId
+        {
+            get { return _instanceId; }
+        }
+
+        public override PSHostUserInterface UI
+        {
+            get { return HostUi; }
+        }
+
+        public override CultureInfo CurrentCulture
+        {
+            get { return _originalCultureInfo; }
+        }
+
+        public override CultureInfo CurrentUICulture
+        {
+            get { return _originalUiCultureInfo; }
+        }
+
+        public void PushRunspace(Runspace runspace)
+        {
+        }
+
+        public void PopRunspace()
+        {
+        }
+
+        public bool IsRunspacePushed
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        ///     The runspace used by the current PowerShell host.
+        /// </summary>
+        public Runspace Runspace
+        {
+            get { return _runspace; }
+        }
+
+        /// <summary>
+        ///     Refreshes the prompt in the REPL window to match the current PowerShell prompt value.
+        /// </summary>
         public void RefreshPrompt()
         {
             if (HostUi != null && HostUi.ReplWindow != null)
-                this.HostUi.ReplWindow.SetOptionValue(ReplOptions.CurrentPrimaryPrompt, GetCurrentDirectory() + "> ");
+                HostUi.ReplWindow.SetOptionValue(ReplOptions.CurrentPrimaryPrompt, GetPrompt());
         }
-
 
         private void SetupExecutionPolicy()
         {
@@ -108,17 +158,19 @@ namespace PowerShellTools.DebugEngine
 
         private void SetExecutionPolicy(ExecutionPolicy policy, ExecutionPolicyScope scope)
         {
-            using (var ps = PowerShell.Create())
+            using (PowerShell ps = PowerShell.Create())
             {
                 ps.Runspace = _runspace;
-                ps.AddCommand("Set-ExecutionPolicy").AddParameter("ExecutionPolicy", policy).AddParameter("Scope", scope);
+                ps.AddCommand("Set-ExecutionPolicy")
+                    .AddParameter("ExecutionPolicy", policy)
+                    .AddParameter("Scope", scope);
                 ps.Invoke();
             }
         }
 
         private ExecutionPolicy GetExecutionPolicy()
         {
-            using (var ps = PowerShell.Create())
+            using (PowerShell ps = PowerShell.Create())
             {
                 ps.Runspace = _runspace;
                 ps.AddCommand("Get-ExecutionPolicy");
@@ -128,7 +180,7 @@ namespace PowerShellTools.DebugEngine
 
         private ExecutionPolicy GetExecutionPolicy(ExecutionPolicyScope scope)
         {
-            using (var ps = PowerShell.Create())
+            using (PowerShell ps = PowerShell.Create())
             {
                 ps.Runspace = _runspace;
                 ps.AddCommand("Get-ExecutionPolicy").AddParameter("Scope", scope);
@@ -136,24 +188,21 @@ namespace PowerShellTools.DebugEngine
             }
         }
 
-        private string GetCurrentDirectory()
+        private string GetPrompt()
         {
             try
             {
-                if (_runspace.RunspaceAvailability == RunspaceAvailability.Available)
-                    return _runspace.SessionStateProxy.Path.CurrentLocation.ProviderPath;
+                using (PowerShell ps = PowerShell.Create())
+                {
+                    ps.Runspace = _runspace;
+                    ps.AddCommand("prompt");
+                    return ps.Invoke<string>().FirstOrDefault();
+                }
             }
             catch
             {
                 return String.Empty;
             }
-
-            return String.Empty;
-        }
-
-        public VSXHost(Runspace runspace)
-        {
-            _runspace = runspace;
         }
 
         public override void SetShouldExit(int exitCode)
@@ -162,7 +211,6 @@ namespace PowerShellTools.DebugEngine
 
         public override void EnterNestedPrompt()
         {
-            
         }
 
         public override void ExitNestedPrompt()
@@ -176,64 +224,19 @@ namespace PowerShellTools.DebugEngine
         public override void NotifyEndApplication()
         {
         }
-
-        public override string Name
-        {
-            get { return "PowerShell Tools for Visual Studio"; }
-        }
-
-        public override Version Version
-        {
-            get { return new Version(1,0,0,0); }
-        }
-
-        public override Guid InstanceId
-        {
-            get { return _instanceId; }
-        }
-
-        public override PSHostUserInterface UI
-        {
-            get { return HostUi; }
-        }
-
-        public override CultureInfo CurrentCulture
-        {
-            get { return originalCultureInfo; }
-        }
-
-        public override CultureInfo CurrentUICulture
-        {
-            get { return originalUICultureInfo; }
-        }
-
-        public void PushRunspace(Runspace runspace)
-        {
-            
-        }
-
-        public void PopRunspace()
-        {
-            
-        }
-
-        public bool IsRunspacePushed
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public Runspace Runspace
-        {
-            get { return _runspace; }
-        }
     }
 
     public class HostUi : PSHostUserInterface
     {
         public IReplWindow ReplWindow { get; set; }
+        public Action<String, int> OutputProgress { get; set; }
+
+        public Action<String> OutputString { get; set; }
+
+        public override PSHostRawUserInterface RawUI
+        {
+            get { return new RawHostUi(); }
+        }
 
         public override string ReadLine()
         {
@@ -242,10 +245,10 @@ namespace PowerShellTools.DebugEngine
 
         public override SecureString ReadLineAsSecureString()
         {
-            var str = Interaction.InputBox("Read-Host", "Read-Host");
+            string str = Interaction.InputBox("Read-Host", "Read-Host");
 
             var s = new SecureString();
-            foreach (var ch in str.ToCharArray())
+            foreach (char ch in str.ToCharArray())
             {
                 s.AppendChar(ch);
             }
@@ -257,10 +260,6 @@ namespace PowerShellTools.DebugEngine
             if (OutputProgress != null)
                 OutputProgress(label, percentage);
         }
-
-        public Action<String, int> OutputProgress { get; set; }
-
-        public Action<String> OutputString { get; set; }
 
         private void TryOutputString(string val)
         {
@@ -307,7 +306,7 @@ namespace PowerShellTools.DebugEngine
 
         public override void WriteProgress(long sourceId, ProgressRecord record)
         {
-            TryOutputProgress(record.Activity + " - "  + record.StatusDescription, record.PercentComplete);
+            TryOutputProgress(record.Activity + " - " + record.StatusDescription, record.PercentComplete);
         }
 
         public override void WriteVerboseLine(string message)
@@ -320,37 +319,38 @@ namespace PowerShellTools.DebugEngine
             TryOutputString("[WARNING] " + message + Environment.NewLine);
         }
 
-        public override Dictionary<string, PSObject> Prompt(string caption, string message, Collection<FieldDescription> descriptions)
+        public override Dictionary<string, PSObject> Prompt(string caption, string message,
+            Collection<FieldDescription> descriptions)
         {
-            
-
             return null;
         }
 
-        public override PSCredential PromptForCredential(string caption, string message, string userName, string targetName)
+        public override PSCredential PromptForCredential(string caption, string message, string userName,
+            string targetName)
         {
-            return CredUIPromptForCredential(caption, message, userName, targetName, PSCredentialTypes.Default, PSCredentialUIOptions.Default, IntPtr.Zero);
+            return CredUIPromptForCredential(caption, message, userName, targetName, PSCredentialTypes.Default,
+                PSCredentialUIOptions.Default, IntPtr.Zero);
         }
 
-        public override PSCredential PromptForCredential(string caption, string message, string userName, string targetName,
-                                                         PSCredentialTypes allowedCredentialTypes, PSCredentialUIOptions options)
+        public override PSCredential PromptForCredential(string caption, string message, string userName,
+            string targetName,
+            PSCredentialTypes allowedCredentialTypes, PSCredentialUIOptions options)
         {
-            return CredUIPromptForCredential(caption, message, userName, targetName, allowedCredentialTypes, options, IntPtr.Zero);
+            return CredUIPromptForCredential(caption, message, userName, targetName, allowedCredentialTypes, options,
+                IntPtr.Zero);
         }
 
-        public override int PromptForChoice(string caption, string message, Collection<ChoiceDescription> choices, int defaultChoice)
+        public override int PromptForChoice(string caption, string message, Collection<ChoiceDescription> choices,
+            int defaultChoice)
         {
             return 0;
         }
 
-        public override PSHostRawUserInterface RawUI
-        {
-            get { return new RawHostUi(); }
-        }
-
 
         // System.Management.Automation.HostUtilities
-        internal static PSCredential CredUIPromptForCredential(string caption, string message, string userName, string targetName, PSCredentialTypes allowedCredentialTypes, PSCredentialUIOptions options, IntPtr parentHWND)
+        internal static PSCredential CredUIPromptForCredential(string caption, string message, string userName,
+            string targetName, PSCredentialTypes allowedCredentialTypes, PSCredentialUIOptions options,
+            IntPtr parentHWND)
         {
             if (string.IsNullOrEmpty(caption))
             {
@@ -362,18 +362,21 @@ namespace PowerShellTools.DebugEngine
             }
             if (caption.Length > 128)
             {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, ResourceStrings.PromptForCredential_InvalidCaption, new object[]{128}));
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
+                    ResourceStrings.PromptForCredential_InvalidCaption, new object[] {128}));
             }
             if (message.Length > 1024)
             {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, ResourceStrings.PromptForCredential_InvalidMessage, new object[]{1024}));
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
+                    ResourceStrings.PromptForCredential_InvalidMessage, new object[] {1024}));
             }
             if (userName != null && userName.Length > 513)
             {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, ResourceStrings.PromptForCredential_InvalidUserName, new object[]{513}));
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
+                    ResourceStrings.PromptForCredential_InvalidUserName, new object[] {513}));
             }
 
-            var cREDUI_INFO = default(CredUI.CREDUI_INFO);
+            CredUI.CREDUI_INFO cREDUI_INFO = default(CredUI.CREDUI_INFO);
             cREDUI_INFO.pszCaptionText = caption;
             cREDUI_INFO.pszMessageText = message;
             var stringBuilder = new StringBuilder(userName, 513);
@@ -382,7 +385,7 @@ namespace PowerShellTools.DebugEngine
             int num = Convert.ToInt32(value);
             cREDUI_INFO.cbSize = Marshal.SizeOf(cREDUI_INFO);
             cREDUI_INFO.hwndParent = parentHWND;
-            CredUI.CREDUI_FLAGS cREDUI_FLAGS = CredUI.CREDUI_FLAGS.DO_NOT_PERSIST;
+            var cREDUI_FLAGS = CredUI.CREDUI_FLAGS.DO_NOT_PERSIST;
             if ((allowedCredentialTypes & PSCredentialTypes.Domain) != PSCredentialTypes.Domain)
             {
                 cREDUI_FLAGS |= CredUI.CREDUI_FLAGS.GENERIC_CREDENTIALS;
@@ -391,10 +394,11 @@ namespace PowerShellTools.DebugEngine
                     cREDUI_FLAGS |= CredUI.CREDUI_FLAGS.ALWAYS_SHOW_UI;
                 }
             }
-            CredUI.CredUIReturnCodes credUIReturnCodes = CredUI.CredUIReturnCodes.ERROR_INVALID_PARAMETER;
+            var credUIReturnCodes = CredUI.CredUIReturnCodes.ERROR_INVALID_PARAMETER;
             if (stringBuilder.Length <= 513 && stringBuilder2.Length <= 256)
             {
-                credUIReturnCodes = CredUI.CredUIPromptForCredentials(ref cREDUI_INFO, targetName, IntPtr.Zero, 0, stringBuilder, 513, stringBuilder2, 256, ref num, cREDUI_FLAGS);
+                credUIReturnCodes = CredUI.CredUIPromptForCredentials(ref cREDUI_INFO, targetName, IntPtr.Zero, 0,
+                    stringBuilder, 513, stringBuilder2, 256, ref num, cREDUI_FLAGS);
             }
             PSCredential result;
             if (credUIReturnCodes == CredUI.CredUIReturnCodes.NO_ERROR)
@@ -404,11 +408,11 @@ namespace PowerShellTools.DebugEngine
                 {
                     text = stringBuilder.ToString();
                 }
-                text = text.TrimStart(new char[]
-		{
-			'\\'
-		});
-                SecureString secureString = new SecureString();
+                text = text.TrimStart(new[]
+                {
+                    '\\'
+                });
+                var secureString = new SecureString();
                 for (int i = 0; i < stringBuilder2.Length; i++)
                 {
                     secureString.AppendChar(stringBuilder2[i]);
@@ -429,11 +433,41 @@ namespace PowerShellTools.DebugEngine
             }
             return result;
         }
-
     }
 
     public class RawHostUi : PSHostRawUserInterface
     {
+        public override ConsoleColor ForegroundColor { get; set; }
+        public override ConsoleColor BackgroundColor { get; set; }
+        public override Coordinates CursorPosition { get; set; }
+        public override Coordinates WindowPosition { get; set; }
+        public override int CursorSize { get; set; }
+
+        public override Size BufferSize
+        {
+            get { return new Size(200, 200); }
+            set { }
+        }
+
+        public override Size WindowSize { get; set; }
+
+        public override Size MaxWindowSize
+        {
+            get { return new Size(100, 100); }
+        }
+
+        public override Size MaxPhysicalWindowSize
+        {
+            get { return new Size(100, 100); }
+        }
+
+        public override bool KeyAvailable
+        {
+            get { return true; }
+        }
+
+        public override string WindowTitle { get; set; }
+
         public override KeyInfo ReadKey(ReadKeyOptions options)
         {
             throw new NotImplementedException();
@@ -441,7 +475,6 @@ namespace PowerShellTools.DebugEngine
 
         public override void FlushInputBuffer()
         {
-            
         }
 
         public override void SetBufferContents(Coordinates origin, BufferCell[,] contents)
@@ -459,46 +492,10 @@ namespace PowerShellTools.DebugEngine
             throw new NotImplementedException();
         }
 
-        public override void ScrollBufferContents(Rectangle source, Coordinates destination, Rectangle clip, BufferCell fill)
+        public override void ScrollBufferContents(Rectangle source, Coordinates destination, Rectangle clip,
+            BufferCell fill)
         {
-           
         }
-
-        public override ConsoleColor ForegroundColor { get; set; }
-        public override ConsoleColor BackgroundColor { get; set; }
-        public override Coordinates CursorPosition { get; set; }
-        public override Coordinates WindowPosition { get; set; }
-        public override int CursorSize { get; set; }
-
-        public override Size BufferSize
-        {
-            get
-            {
-                return new Size(200,200);
-            }
-            set
-            {
-                
-            }
-        }
-        public override Size WindowSize { get; set; }
-
-        public override Size MaxWindowSize
-        {
-            get { return new Size(100, 100); }
-        }
-
-        public override Size MaxPhysicalWindowSize
-        {
-            get { return new Size(100,100);}
-        }
-
-        public override bool KeyAvailable
-        {
-            get { return true; }
-        }
-
-        public override string WindowTitle { get; set; }
     }
 
     public interface IOutputWriter
