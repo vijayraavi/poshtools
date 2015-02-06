801932fc2c4aae9017775f5c6d9c76270c15041c
EricMSFT <zhiyd@microsoft.com>
2015-02-05 18:18:03 -0800

    Refactor servicebehavior impl

--- PowerShellTools.HostService/PowershellServiceHostBehavior.cs
+++ PowerShellTools.HostService/PowershellServiceHostBehavior.cs
@@ -12,13 +12,18 @@ using System.Threading.Tasks;
 
 namespace PowerShellTools.HostService
 {
+    /// <summary>
+    /// In WCF, unhandled excpetion crashes the servcie, leaving the channel into fault state, which is basically requiring client to re-instantiate proxy in order to continue using the service.
+    /// As a result, WCF provides the ability to configure a service to return information from unhandled exceptions.
+    /// This is implemetaion of the generic error handler for entire powershell wcf services, by exposing it as a service behavior attribute, so that it is developer friendly
+    /// </summary>
     class PowershellServiceHostBehavior : Attribute, IErrorHandler, IServiceBehavior
     {
         #region IErrorHandler Members
 
         public bool HandleError(Exception error)
         {
-            // Log the error here
+            // Log the error details on server side
             ServiceCommon.Log("PowershellHostService:", error.ToString());
 
             // Let the other ErrorHandler do their jobs
@@ -27,13 +32,14 @@ namespace PowerShellTools.HostService
 
         public void ProvideFault(Exception error, MessageVersion version, ref Message fault)
         {
+            // If the error is intented to be send to client, we just let it be
             if (error is FaultException)
                 return;
 
             // Creates the exception we want to send back to the client
             var exception = new FaultException<PowershellHostServiceExceptionDetails>(
-            PowershellHostServiceExceptionDetails.Default,
-            new FaultReason(PowershellHostServiceExceptionDetails.Default.Message));
+                PowershellHostServiceExceptionDetails.Default,
+                new FaultReason(PowershellHostServiceExceptionDetails.Default.Message));
 
             // Creates a message fault
             var messageFault = exception.CreateMessageFault();
@@ -46,13 +52,11 @@ namespace PowerShellTools.HostService
 
         #region IServiceBehavior Members
 
-        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
-        {
-        }
-
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters) {}
+        
         public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
         {
-            // Adds a TimeServiceErrorHandler to each ChannelDispatcher
+            // Adds a PowershellServiceHostBehavior to each ChannelDispatcher
             foreach (var channelDispatcherBase in serviceHostBase.ChannelDispatchers)
             {
                 var channelDispatcher = channelDispatcherBase as ChannelDispatcher;
@@ -60,10 +64,8 @@ namespace PowerShellTools.HostService
             }
         }
 
-        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-        }
-
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase) { }
+        
         #endregion
     }
 }
