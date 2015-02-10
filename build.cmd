13a462e439fefac9028c1809954f479f96ec0a47
Huanhuan Sun <husun@microsoft.com>
2015-02-10 12:13:09 -0800

    build script defaults to build Dev12 with 14.0 specified as parameter, it will build Dev14

--- build.cmd
+++ build.cmd
@@ -1,2 +1,5 @@
-msbuild "%POSHTOOLROOT%\PowerShellTools.sln" /m /filelogger /verbosity:normal /p:Configuration=Release;VisualStudioVersion=12.0
+set VsVersion=%1
+if "%1" == "" set VsVersion=12.0
+
+msbuild "%POSHTOOLROOT%\PowerShellTools.sln" /m /filelogger /verbosity:normal /p:Configuration=Release;VisualStudioVersion=%VsVersion%
 
