2716614049e372e2f20a443b2e84d4cd83113f95
Andre Sayre <Andre.Sayre@microsoft.com>
2015-02-18 16:23:38 -0800

    Additional fixes for #186.  Properly retrieves the path if no active document.

--- PowerShellTools/Commands/ExecuteAsScriptCommand.cs
+++ PowerShellTools/Commands/ExecuteAsScriptCommand.cs
@@ -56,8 +56,8 @@ namespace PowerShellTools.Commands
         {
             var selectedItem = GetSelectedItem(dte);
 
-            if (selectedItem != null && selectedItem.ProjectItem != null && selectedItem.ProjectItem.Document != null)
-                return selectedItem.ProjectItem.Document.FullName;
+            if (selectedItem != null && selectedItem.ProjectItem != null)
+                return GetPathOfProjectItem(selectedItem.ProjectItem);
 
             return null;
         }
@@ -77,6 +77,31 @@ namespace PowerShellTools.Commands
             if (applicationObject.SelectedItems.Count == 1)
                 return applicationObject.SelectedItems.Item(1);
             return null;
+        }
+
+        /// <summary>
+        /// Returns the path of the project item.  Can return null if path is not found/applicable.
+        /// </summary>
+        /// <param name="projItem">The project Item</param>
+        /// <returns>A string representing the path of the project item.  returns null if path is not found.</returns>
+        private static string GetPathOfProjectItem(ProjectItem projItem)
+        {
+            Debug.Assert(projItem != null, "projItem shouldn't be null");
+
+            Properties projProperties = projItem.Properties;
+
+            try
+            {
+                string path = projProperties.Item("FullPath").Value as string;    //Item throws if not found.
+
+                Debug.Assert(path != null, "Path isn't a string");
+
+                return path;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 
