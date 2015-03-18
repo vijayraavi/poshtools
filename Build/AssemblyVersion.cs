c2aae180819ab6cd15feda85c34ab5e7c5100935
AndreSayreMSFT <Andre.Sayre@microsoft.com>
2015-03-18 16:27:00 -0700

    Sets default versions to something above the incorrect versions which were a hold-over from [N|P]TVS.  Also sets all build configs to AnyCPU.

--- Build/AssemblyVersion.cs
+++ Build/AssemblyVersion.cs
@@ -10,45 +10,7 @@ using System.Runtime.InteropServices;
 // Properties\AssemblyInfo.cs file and remove any lines duplicating the ones below.
 // (See also AssemblyInfoCommon.cs in this same directory.)
 
-[assembly: AssemblyVersion("3.4.0.0")] 
-[assembly: AssemblyFileVersion("3.4.5.6")] 
-[assembly: AssemblyInformationalVersion("3.4.5.6")]
-
-class AssemblyVersionInfo {
-    // This version string (and the comments for StableVersion and Version)
-    // should be updated manually between major releases.
-    // Servicing branches should retain the value
-    public const string ReleaseVersion = "2.0";
-    // This version string (and the comment for StableVersion) should be
-    // updated manually between minor releases.
-    // Servicing branches should retain the value
-    public const string MinorVersion = "1";
-
-    // This version should never change from "4100.00"; BuildRelease.ps1
-    // will replace it with a generated value.
-    public const string BuildNumber = "4100.00";
-
-#if DEV10
-    public const string VSMajorVersion = "10";
-    const string VSVersionSuffix = "2010";
-#elif DEV11
-    public const string VSMajorVersion = "11";
-    const string VSVersionSuffix = "2012";
-#elif DEV12
-    public const string VSMajorVersion = "12";
-    const string VSVersionSuffix = "2013";
-#elif DEV14
-    public const string VSMajorVersion = "14";
-    const string VSVersionSuffix = "2015";
-#else
-#error Unrecognized VS Version.
-#endif
-
-    public const string VSVersion = VSMajorVersion + ".0";
-
-    // Defaults to "2.0.1.(2010|2012|2013)"
-    public const string StableVersion = ReleaseVersion + "." + MinorVersion + "." + VSVersionSuffix;
-
-    // Defaults to "2.0.4100.00"
-    public const string Version = ReleaseVersion + "." + BuildNumber;
-}
+// These are used only on the local dev builds.  
+[assembly: AssemblyVersion("1.2.3.4")] 
+[assembly: AssemblyFileVersion("1.2.3.4")] 
+[assembly: AssemblyInformationalVersion("1.2.3.4 - DEV BUILD")]
