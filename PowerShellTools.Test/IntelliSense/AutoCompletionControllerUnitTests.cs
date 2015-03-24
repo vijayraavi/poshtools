e9bfdad8e6651124984b53256e6a8c2c1ca3d617
Huanhuan Sun <husun@microsoft.com>
2015-03-24 14:10:35 -0700

    unittests

---
+++ PowerShellTools.Test/IntelliSense/AutoCompletionControllerUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Moq;
+using PowerShellTools.Intellisense;
+
+namespace PowerShellTools.Test.IntelliSense
+{
+    [TestClass]
+    public class AutoCompletionControllerUnitTests
+    {
+        private string _mockedScript;
+        private int _mockedCaret;
+
+        [TestMethod]
+        public void TestCompleteBrace()
+        {
+            TestCompleteBraceOrQuotesHelper('(', @"MyFunc", 6, @"MyFunc()", 7);
+        }
+
+        [TestMethod]
+        public void TestCompleteQuotes()
+        {
+            TestCompleteBraceOrQuotesHelper('\"', @"MyFunc()", 7, @"MyFunc("""")", 8);
+        }
+
+        private void TestCompleteBraceOrQuotesHelper(char leftBraceOrQuotes, string initialScript, int initialCaret, string expectedScript, int expectedCaret)
+        {
+            Mock<ITextView> textView =  new Mock<ITextView>();
+            var editorOperations = EditorOperationsMock();
+            var textUndoHistory = TextUndoHistoryMock();
+            Mock<SVsServiceProvider> serviceProvider = new Mock<SVsServiceProvider>();
+            AutoCompletionController autoCompletionController = new AutoCompletionController(textView.Object,
+                                                                                              editorOperations.Object,
+                                                                                              textUndoHistory.Object,
+                                                                                              serviceProvider.Object);
+            _mockedScript = initialScript;
+            _mockedCaret = initialCaret;
+            autoCompletionController.CompleteBraceOrQuotes(leftBraceOrQuotes);
+            Assert.AreEqual<int>(expectedCaret, _mockedCaret);
+            Assert.AreEqual<string>(expectedScript, _mockedScript);
+        }
+
+        private void TextViewMockHelper(Mock<ITextView> textView)
+        {
+
+        }
+
+        private Mock<IEditorOperations> EditorOperationsMock()
+        {
+            Mock<Microsoft.VisualStudio.Text.Operations.IEditorOperations> editorOperations = new Mock<IEditorOperations>();
+            editorOperations.Setup(m => m.AddBeforeTextBufferChangePrimitive()).Callback(() => { });
+            editorOperations.Setup(m => m.AddAfterTextBufferChangePrimitive()).Callback(() => { });
+            editorOperations.Setup(m => m.InsertText(It.IsAny<string>()))
+                            .Callback<string>(p =>
+                                                {
+                                                    _mockedScript = _mockedScript.Insert(_mockedCaret, p);
+                                                    _mockedCaret++;
+                                                });
+            editorOperations.Setup(m => m.MoveToPreviousCharacter(false))
+                            .Callback(() =>
+                                        {
+                                            _mockedCaret--;
+                                        });
+            return editorOperations;
+        }
+
+        private Mock<ITextUndoHistory> TextUndoHistoryMock()
+        {
+            Mock<ITextUndoHistory> textUndoHistory = new Mock<ITextUndoHistory>();
+            Mock<ITextUndoTransaction> textUndoTransactioin = new Mock<ITextUndoTransaction>();
+            textUndoTransactioin.Setup(m => m.Complete()).Callback(() => {});
+            textUndoHistory.Setup(m => m.CreateTransaction(It.IsAny<string>())).Returns(() => textUndoTransactioin.Object);
+            return textUndoHistory;
+        }
+    }
+}
---
+++ PowerShellTools.Test/IntelliSense/CommentAreaUnitTests.cs
@@ -0,0 +1,118 @@
+﻿using System.Management.Automation.Language;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using Moq;
+using PowerShellTools.Classification;
+using PowerShellTools.Intellisense;
+
+namespace PowerShellTools.Test.IntelliSense
+{
+    [TestClass]
+    public class CommentAreaUnitTests
+    {
+        private AutoCompletionController _braceCompletionController;
+        private Mock<ITextView> _textView;
+        private Mock<Microsoft.VisualStudio.Text.Operations.IEditorOperations> _editorOptions;
+        private Mock<ITextUndoHistory> _undoHistory;
+        private Mock<SVsServiceProvider> _serviceProvider;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _textView = new Mock<ITextView>();
+            _editorOptions = new Mock<IEditorOperations>();
+            _undoHistory = new Mock<ITextUndoHistory>();
+            _serviceProvider = new Mock<SVsServiceProvider>();
+            _braceCompletionController = new AutoCompletionController(_textView.Object,
+                                                                       _editorOptions.Object,
+                                                                       _undoHistory.Object,
+                                                                       _serviceProvider.Object);
+        }
+
+        [TestMethod]
+        public void TestEmptyScript()
+        {
+            IsInCommentAreaTestHelper("", 0, false);
+        }
+
+        [TestMethod]
+        public void TestNoCommentScript()
+        {
+            string script = @"param(
+                                [parameter(Mandatory=$true)]
+                                [string]$someStr
+                                )";
+            IsInCommentAreaTestHelper(script, 0, false);
+            IsInCommentAreaTestHelper(script, 10, false);
+            IsInCommentAreaTestHelper(script, script.Length, false);
+        }
+
+        [TestMethod]
+        public void TestAllLineCommentScript()
+        {
+            string script = @"#param(
+                              #  [parameter(Mandatory=$true)]
+                              #  [string]$someStr
+                              #  )";
+            IsInCommentAreaTestHelper(script, 0, true);
+            IsInCommentAreaTestHelper(script, 1, true);
+            IsInCommentAreaTestHelper(script, 50, true);
+            IsInCommentAreaTestHelper(script, script.Length, true);
+        }
+
+        [TestMethod]
+        public void TestAllBlockCommentScript()
+        {
+            string script = @"<# param(
+                                 [parameter(Mandatory=$true)]
+                                 [string]$someStr
+                                 )
+                              #>";
+            IsInCommentAreaTestHelper(script, 0, true);
+            IsInCommentAreaTestHelper(script, 20, true);
+            IsInCommentAreaTestHelper(script, script.Length, true);
+        }
+
+        [TestMethod]
+        public void TestNormalCommentScript()
+        {
+            string script = @"<# This is a block comment
+                              #>
+                              param(
+                                [parameter(Mandatory=$true)]
+                                [string]$someStr
+                                )
+                              # This is line comment 1
+                              # This is line comment 2";
+            IsInCommentAreaTestHelper(script, 0, true);
+            IsInCommentAreaTestHelper(script, 10, true);
+            IsInCommentAreaTestHelper(script, 100, false);
+            IsInCommentAreaTestHelper(script, 300, true);
+            IsInCommentAreaTestHelper(script, script.Length, true);
+        }
+
+        private void IsInCommentAreaTestHelper(string script, int caretPosition, bool expected)
+        {
+            Token[] tokens;
+            ParseError[] errors;
+            Parser.ParseInput(script, out tokens, out errors);
+
+            Mock<ITextBuffer> textBuffer = new Mock<ITextBuffer>();
+            TextBufferMockHelper(textBuffer, tokens);
+            bool actual = _braceCompletionController.IsInCommentArea(caretPosition, textBuffer.Object);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private void TextBufferMockHelper(Mock<ITextBuffer> textBuffer, Token[] tokens)
+        {
+            PropertyCollection pc = new PropertyCollection();
+            pc.AddProperty(BufferProperties.Tokens, tokens);
+            textBuffer.Setup(m => m.Properties).Returns(pc);
+        }
+    }
+}
