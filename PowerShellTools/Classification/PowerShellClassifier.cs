ec7d7042210c93c1e125e63113fd887ece86f9e0
Huanhuan Sun <husun@microsoft.com>
2015-03-30 20:25:57 -0700

    Null check

--- PowerShellTools/Classification/PowerShellClassifier.cs
+++ PowerShellTools/Classification/PowerShellClassifier.cs
@@ -134,8 +134,9 @@ namespace PowerShellTools.Classification
 
 		internal static void AddTokenClassifications(ITextBuffer buffer, SnapshotSpan span, List<ClassificationSpan> classifications, Span? lastClassificationSpan, IClassificationType gapType)
 		{
-            var spans = new List<ClassificationInfo>();
-            if (!buffer.Properties.TryGetProperty(BufferProperties.TokenSpans, out spans))
+            List<ClassificationInfo> spans = null;
+            if (!buffer.Properties.TryGetProperty(BufferProperties.TokenSpans, out spans)
+                || spans == null)
             {
                 return;
             }
