75cdf707ec828e23b76e690195521ae36df0f50c
jinglou- <jinglou@microsoft.com>
2015-04-17 11:59:18 -0700

    Fixed #391 replace with a fwlink

--- PowerShellTools/Templates/Projects/Module.test.ps1
+++ PowerShellTools/Templates/Projects/Module.test.ps1
@@ -1,7 +1,7 @@
 #
 # This is a PowerShell Unit Test file.
 # You need a unit test framework such as Pester to run PowerShell Unit tests. 
-# You can download Pester from https://github.com/pester/Pester 
+# You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
 
 Describe "Get-Function" {
