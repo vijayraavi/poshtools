d2125013d233e55f75461961b4492965e72812e6
Huanhuan Sun <husun@microsoft.com>
2015-05-04 15:38:19 -0700

    add a file

---
+++ PowerShellTools/Classification/PowerShellParamtersUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+
+namespace PowerShellTools.Classification
+{
+    internal static class PowerShellParamtersUtilities
+    {
+        public static bool HasParamBlock(ITextBuffer buffer)
+        {
+
+        }
+    }
+}
