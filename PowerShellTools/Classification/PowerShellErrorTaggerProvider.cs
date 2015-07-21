a1bf863bbb420716ce1f848074c224159ea25cd9
Huanhuan Sun <husun@microsoft.com>
2015-07-21 14:49:15 -0700

    Add dependency validation for editor components

--- PowerShellTools/Classification/PowerShellErrorTaggerProvider.cs
+++ PowerShellTools/Classification/PowerShellErrorTaggerProvider.cs
@@ -1,22 +1,22 @@
-using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
 
 namespace PowerShellTools.Classification
 {
     [ContentType("TextOutput"), Export(typeof(IViewTaggerProvider)), TagType(typeof(ErrorTag)), ContentType("PowerShell")]
     internal class PowerShellErrorTaggerProvider : IViewTaggerProvider
     {
-	[Import]
-	internal IDependencyValidator _validator;
+        [Import]
+        private IDependencyValidator _validator;
 
-	public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
-	{
-	    if (!_validator.Validate()) return null;
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            if (!_validator.Validate()) return null;
 
-	    return buffer.Properties.GetOrCreateSingletonProperty(typeof(PowerShellErrorTagger), () => new PowerShellErrorTagger(buffer) as ITagger<T>);
-	}
+            return buffer.Properties.GetOrCreateSingletonProperty(typeof(PowerShellErrorTagger), () => new PowerShellErrorTagger(buffer) as ITagger<T>);
+        }
     }
 }
